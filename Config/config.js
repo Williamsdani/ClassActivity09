0f960b29c2a2ba744615753f30416cb885766677
Williams Daniel <108428642+Williamsdani@users.noreply.github.com>
2023-07-05 17:09:35 +0100

    push

--- Config/config.js
+++ Config/config.js
@@ -1,7 +1,3 @@
-// const mongoose=require('mongoose')
-// const express=require('express')
-// mongoose.connect("mongodb+srv://wd7576383:XnjvLvtFWIvQWXWA@cluster0.tjoqfnu.mongodb.net/").then(()=>{console.log('sucessful')})
-// .catch((err)=>{console.log(err.message);});
 
 require('dotenv').config();
 const mongoose = require('mongoose');
